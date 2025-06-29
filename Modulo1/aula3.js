9a07ee8ddcb31b9afa2ef813b762d271150be5ec
Gabriel Cordeiro Valadares <120654513+Gabrie1Valadares@users.noreply.github.com>
2025-06-29 17:27:05 -0300

    Exercício com variáveis

---
+++ Modulo1/aula3.js
@@ -0,0 +1,34 @@
+/*// Alert, confirm e prompt (Navegador)
+
+//Exercício com variáveis 
+let num1 = prompt("Digite um número: ");
+let num2 = prompt("Digite outro número: ");
+let resultado;
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+resultado = num1 + num2;
+
+alert(`O resultado é ${resultado}`);
+
+ */
+
+//Exercício com variáveis sobre esculpa de variáveis
+
+let varA = "A";
+let varB = "B";
+let varC = "C";
+
+/*
+//Essa seria umas das formas re resolver esse exercício
+const varD = varA;
+varA = varB;
+varB = varC;
+varC = varD;
+*/
+
+//Essa seria um forma mais moderna para resolver o exercício
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);
