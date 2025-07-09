aa35b1d8c7471362dd5fb1a887c5dbefd96a0d51
Gabriel Cordeiro Valadares <120654513+Gabrie1Valadares@users.noreply.github.com>
2025-07-09 20:20:58 -0300

    Objeto Math

---
+++ Modulo1/aula7.js
@@ -0,0 +1,21 @@
+//Objeto Math
+
+let num1 = 5.48;
+let num2 = 9;
+//Podemos arredonda os número tanto para cima quanto para baixo
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+
+//nesse caso o valor sera arredondado para o valor mais proximo
+//let num2 = Math.round(num1);
+
+//Para vermos qual o maior ou menor número de uma sequencia podemos utilizar as tag:
+//console.log(Math.max(1,2,3,5,10,31,44,23,44,55,66));
+//console.log(Math.main(1,2,3,5,10,31,44,23,-44,-55,-66));
+
+//Podemos gera um número aleatório
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(aleatorio);
+
+//Importa no js um número pode ser dividido por 0 é essa conta consta como true
+console.log(100 / 0);
