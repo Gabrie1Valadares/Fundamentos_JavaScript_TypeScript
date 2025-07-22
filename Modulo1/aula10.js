ab2e4bc5cb9988c8eb34d19e70e19f0fccb5e4f9
Gabriel Cordeiro Valadares <120654513+Gabrie1Valadares@users.noreply.github.com>
2025-07-22 00:44:06 -0300

    Funções em JS / Introdução

---
+++ Modulo1/aula10.js
@@ -0,0 +1,33 @@
+//Funções em JS / Introdução
+/**
+ 
+//Função: Um bloco de código que executa uma tarefa específica.
+//return: Palavra-chave usada para devolver um valor da função para quem a chamou
+
+function saudacao(nome) {
+  console.log(`Bom dia ${nome}! `);
+  return 1234;
+}
+
+const variavel = saudacao(w"Ana");
+
+console.log(variavel);
+ 
+
+function somar(a, b) {
+  return a + b;
+}
+
+let resultado = somar(5, 3);
+console.log(resultado); // Saída: 8
+
+//Podemos cria um funções anonima
+
+const raiz = function (n) {
+  return n ** 0.5;
+};
+
+console.log(raiz(9));
+*/
+
+// Existe tbm uma jeito mais atual de cria um função chamada arrow function
